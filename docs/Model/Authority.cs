2d62ec2307c818147dbf1b6ad95fd14798cb0958
geverson.souza <geverson.souza@odontosystem.com.br>
2024-11-30 19:12:20 -0300

    Ajustes para entrega

--- docs/Model/Authority.cs
+++ docs/Model/Authority.cs
@@ -2,11 +2,13 @@ namespace Docs
 {
     public class Authority
     {
+
         public  long auth_pk_id {get; set;}
         public  String auth_description {get; set;}
         public   Boolean auth_status {get; set;}
         public  String auth_screen {get; set;}
         public  String auth_function {get; set;}
         public  User usuario {get; set; }
+        public Authority CriarPermissao {get; set;}
     }
 }
